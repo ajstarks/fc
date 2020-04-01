600cb7569eee68f01c57cda9f3e6a934e2f427a8
Anthony Starks <ajstarks@gmail.com>
2020-04-01 13:48:30 -0400

    add TextWidth, unexport "I" methods

--- fc.go
+++ fc.go
@@ -22,7 +22,7 @@ type Canvas struct {
 func NewCanvas(name string, w, h int) Canvas {
 	c := Canvas{
 		Window:    app.New().NewWindow(name),
-		Container: fyne.NewContainer(IRect(w/2, h/2, w, h, color.RGBA{0, 0, 0, 255})),
+		Container: fyne.NewContainer(iRect(w/2, h/2, w, h, color.RGBA{0, 0, 0, 255})),
 		Width:     float64(w),
 		Height:    float64(h),
 	}
@@ -59,7 +59,7 @@ func dimen(xp, yp, w, h float64) (float64, float64) {
 
 // AbsStart initiates the canvas
 func AbsStart(name string, w, h int) (fyne.Window, *fyne.Container) {
-	return app.New().NewWindow(name), fyne.NewContainer(IRect(w/2, h/2, w, h, color.RGBA{0, 0, 0, 255}))
+	return app.New().NewWindow(name), fyne.NewContainer(iRect(w/2, h/2, w, h, color.RGBA{0, 0, 0, 255}))
 }
 
 // EndRun shows the content and runs the app
@@ -81,8 +81,8 @@ func AbsEndRun(window fyne.Window, c *fyne.Container, w, h int) {
 	window.ShowAndRun()
 }
 
-// IText places text
-func IText(x, y int, s string, size int, color color.RGBA) *canvas.Text {
+// iText places text
+func iText(x, y int, s string, size int, color color.RGBA) *canvas.Text {
 	t := &canvas.Text{Text: s, Color: color, TextSize: size}
 	adj := size / 5
 	p := fyne.Position{X: x, Y: y - (size + adj)}
@@ -90,38 +90,38 @@ func IText(x, y int, s string, size int, color color.RGBA) *canvas.Text {
 	return t
 }
 
-// ITextMid centers text
-func ITextMid(x, y int, s string, size int, color color.RGBA) *canvas.Text {
-	t := IText(x, y, s, size, color)
+// iTextMid centers text
+func iTextMid(x, y int, s string, size int, color color.RGBA) *canvas.Text {
+	t := iText(x, y, s, size, color)
 	t.Alignment = fyne.TextAlignCenter
 	return t
 }
 
-// ITextEnd end-aligns text
-func ITextEnd(x, y int, s string, size int, color color.RGBA) *canvas.Text {
-	t := IText(x, y, s, size, color)
+// iTextEnd end-aligns text
+func iTextEnd(x, y int, s string, size int, color color.RGBA) *canvas.Text {
+	t := iText(x, y, s, size, color)
 	t.Alignment = fyne.TextAlignTrailing
 	return t
 }
 
-// ILine draws a line
-func ILine(x1, y1, x2, y2 int, size float32, color color.RGBA) *canvas.Line {
+// iLine draws a line
+func iLine(x1, y1, x2, y2 int, size float32, color color.RGBA) *canvas.Line {
 	p1 := fyne.Position{X: x1, Y: y1}
 	p2 := fyne.Position{X: x2, Y: y2}
 	l := &canvas.Line{StrokeColor: color, StrokeWidth: size, Position1: p1, Position2: p2}
 	return l
 }
 
-// ICircle draws a circle centered at (x,y)
-func ICircle(x, y, r int, color color.RGBA) *canvas.Circle {
+// iCircle draws a circle centered at (x,y)
+func iCircle(x, y, r int, color color.RGBA) *canvas.Circle {
 	p1 := fyne.Position{X: x - r, Y: y - r}
 	p2 := fyne.Position{X: x + r, Y: y + r}
 	c := &canvas.Circle{FillColor: color, Position1: p1, Position2: p2}
 	return c
 }
 
-// ICornerRect makes a rectangle
-func ICornerRect(x, y, w, h int, color color.RGBA) *canvas.Rectangle {
+// iCornerRect makes a rectangle
+func iCornerRect(x, y, w, h int, color color.RGBA) *canvas.Rectangle {
 	r := &canvas.Rectangle{FillColor: color}
 	r.Move(fyne.Position{X: x, Y: y})
 	r.Resize(fyne.Size{Width: w, Height: h})
@@ -129,23 +129,23 @@ func ICornerRect(x, y, w, h int, color color.RGBA) *canvas.Rectangle {
 }
 
 // IRect makes a rectangle centered at x,y
-func IRect(x, y, w, h int, color color.RGBA) *canvas.Rectangle {
+func iRect(x, y, w, h int, color color.RGBA) *canvas.Rectangle {
 	r := &canvas.Rectangle{FillColor: color}
 	r.Move(fyne.Position{X: x - (w / 2), Y: y - (h / 2)})
 	r.Resize(fyne.Size{Width: w, Height: h})
 	return r
 }
 
-// IImage places the image centered at x, y
-func IImage(x, y, w, h int, name string) *canvas.Image {
+// iImage places the image centered at x, y
+func iImage(x, y, w, h int, name string) *canvas.Image {
 	i := canvas.NewImageFromFile(name)
 	i.Move(fyne.Position{X: x - (w / 2), Y: y - (h / 2)})
 	i.Resize(fyne.Size{Width: w, Height: h})
 	return i
 }
 
-// ICornerImage places the image centered at x, y
-func ICornerImage(x, y, w, h int, name string) *canvas.Image {
+// iCornerImage places the image centered at x, y
+func iCornerImage(x, y, w, h int, name string) *canvas.Image {
 	i := canvas.NewImageFromFile(name)
 	i.Move(fyne.Position{X: x, Y: y})
 	i.Resize(fyne.Size{Width: w, Height: h})
@@ -155,132 +155,138 @@ func ICornerImage(x, y, w, h int, name string) *canvas.Image {
 // container methods, Absoulte coordinates
 
 // AbsText places text within a container
-func AbsText(c *fyne.Container, x, y int, s string, size int, color color.RGBA) {
+func AbsText(cont *fyne.Container, x, y int, s string, size int, color color.RGBA) {
 	t := &canvas.Text{Text: s, Color: color, TextSize: size}
 	adj := size / 5
 	p := fyne.Position{X: x, Y: y - (size + adj)}
 	t.Move(p)
-	c.AddObject(t)
+	cont.AddObject(t)
 }
 
 // AbsTextMid centers text within a container
-func AbsTextMid(c *fyne.Container, x, y int, s string, size int, color color.RGBA) {
-	t := IText(x, y, s, size, color)
+func AbsTextMid(cont *fyne.Container, x, y int, s string, size int, color color.RGBA) {
+	t := iText(x, y, s, size, color)
 	t.Alignment = fyne.TextAlignCenter
-	c.AddObject(t)
+	cont.AddObject(t)
 }
 
 // AbsTextEnd end-aligns text within a container
-func AbsTextEnd(c *fyne.Container, x, y int, s string, size int, color color.RGBA) {
-	t := IText(x, y, s, size, color)
+func AbsTextEnd(cont *fyne.Container, x, y int, s string, size int, color color.RGBA) {
+	t := iText(x, y, s, size, color)
 	t.Alignment = fyne.TextAlignTrailing
-	c.AddObject(t)
+	cont.AddObject(t)
 }
 
 // AbsLine draws a line within a container
-func AbsLine(c *fyne.Container, x1, y1, x2, y2 int, size float32, color color.RGBA) {
+func AbsLine(cont *fyne.Container, x1, y1, x2, y2 int, size float32, color color.RGBA) {
 	p1 := fyne.Position{X: x1, Y: y1}
 	p2 := fyne.Position{X: x2, Y: y2}
-	c.AddObject(&canvas.Line{StrokeColor: color, StrokeWidth: size, Position1: p1, Position2: p2})
+	cont.AddObject(&canvas.Line{StrokeColor: color, StrokeWidth: size, Position1: p1, Position2: p2})
 }
 
 // AbsCircle is a containerized circle within a container
-func AbsCircle(c *fyne.Container, x, y, r int, color color.RGBA) {
+func AbsCircle(cont *fyne.Container, x, y, r int, color color.RGBA) {
 	p1 := fyne.Position{X: x - r, Y: y - r}
 	p2 := fyne.Position{X: x + r, Y: y + r}
-	c.AddObject(&canvas.Circle{FillColor: color, Position1: p1, Position2: p2})
+	cont.AddObject(&canvas.Circle{FillColor: color, Position1: p1, Position2: p2})
 }
 
 // AbsCornerRect makes a rectangle within a container
-func AbsCornerRect(c *fyne.Container, x, y, w, h int, color color.RGBA) {
+func AbsCornerRect(cont *fyne.Container, x, y, w, h int, color color.RGBA) {
 	r := &canvas.Rectangle{FillColor: color}
 	r.Move(fyne.Position{X: x, Y: y})
 	r.Resize(fyne.Size{Width: w, Height: h})
-	c.AddObject(r)
+	cont.AddObject(r)
 }
 
 // AbsRect makes a rectangle centered at x,y within a container
-func AbsRect(c *fyne.Container, x, y, w, h int, color color.RGBA) {
+func AbsRect(cont *fyne.Container, x, y, w, h int, color color.RGBA) {
 	r := &canvas.Rectangle{FillColor: color}
 	r.Move(fyne.Position{X: x - (w / 2), Y: y - (h / 2)})
 	r.Resize(fyne.Size{Width: w, Height: h})
-	c.AddObject(r)
+	cont.AddObject(r)
 }
 
 // AbsImage places the image centered at x, y within a container
-func AbsImage(c *fyne.Container, x, y, w, h int, name string) {
+func AbsImage(cont *fyne.Container, x, y, w, h int, name string) {
 	i := canvas.NewImageFromFile(name)
 	i.Move(fyne.Position{X: x - (w / 2), Y: y - (h / 2)})
 	i.Resize(fyne.Size{Width: w, Height: h})
-	c.AddObject(i)
+	cont.AddObject(i)
 }
 
 // AbsCornerImage places the image centered at x, y within a container
-func AbsCornerImage(c *fyne.Container, x, y, w, h int, name string) {
+func AbsCornerImage(cont *fyne.Container, x, y, w, h int, name string) {
 	i := canvas.NewImageFromFile(name)
 	i.Move(fyne.Position{X: x, Y: y})
 	i.Resize(fyne.Size{Width: w, Height: h})
-	c.AddObject(i)
+	cont.AddObject(i)
 }
 
 //
 // container methods, Percent coordinates
 //
 
+// TextWidth returns the width of a string
+func (c *Canvas) TextWidth(s string, size float64) float64 {
+	t := &canvas.Text{Text: s, TextSize: int(pct(size, c.Width))}
+	return float64(t.MinSize().Width)
+}
+
 // Text places text within a container, using percent coordinates
-func (canvas *Canvas) Text(x, y float64, size float64, s string, color color.RGBA) {
-	x, y = dimen(x, y, canvas.Width, canvas.Height)
-	size = pct(size, canvas.Width)
-	AbsText(canvas.Container, int(x), int(y), s, int(size), color)
+func (c *Canvas) Text(x, y float64, size float64, s string, color color.RGBA) {
+	x, y = dimen(x, y, c.Width, c.Height)
+	size = pct(size, c.Width)
+	AbsText(c.Container, int(x), int(y), s, int(size), color)
 }
 
 // CText places centered text using percent coordinates
-func (canvas *Canvas) CText(x, y float64, size float64, s string, color color.RGBA) {
-	x, y = dimen(x, y, canvas.Width, canvas.Height)
-	size = pct(size, canvas.Width)
-	AbsTextMid(canvas.Container, int(x), int(y), s, int(size), color)
+func (c *Canvas) CText(x, y float64, size float64, s string, color color.RGBA) {
+	x, y = dimen(x, y, c.Width, c.Height)
+	size = pct(size, c.Width)
+	AbsTextMid(c.Container, int(x), int(y), s, int(size), color)
 }
 
 // EText places end-aligned text within a container, using percent coordinates
-func (canvas *Canvas) EText(x, y float64, size float64, s string, color color.RGBA) {
-	x, y = dimen(x, y, canvas.Width, canvas.Height)
-	size = pct(size, canvas.Width)
-	AbsTextEnd(canvas.Container, int(x), int(y), s, int(size), color)
+func (c *Canvas) EText(x, y float64, size float64, s string, color color.RGBA) {
+	x, y = dimen(x, y, c.Width, c.Height)
+	size = pct(size, c.Width)
+	AbsTextEnd(c.Container, int(x), int(y), s, int(size), color)
 }
 
 // Circle places a circle within a container, using percent coordinates
-func (canvas *Canvas) Circle(x, y, r float64, color color.RGBA) {
-	x, y = dimen(x, y, canvas.Width, canvas.Height)
-	r = pct(r, canvas.Width)
-	AbsCircle(canvas.Container, int(x), int(y), int(r/2), color)
+func (c *Canvas) Circle(x, y, r float64, color color.RGBA) {
+	x, y = dimen(x, y, c.Width, c.Height)
+	r = pct(r, c.Width)
+	AbsCircle(c.Container, int(x), int(y), int(r/2), color)
 }
 
 // Line places a line within a container, using percent coordinates
-func (canvas *Canvas) Line(x1, y1, x2, y2, size float64, color color.RGBA) {
-	x1, y1 = dimen(x1, y1, canvas.Width, canvas.Height)
-	x2, y2 = dimen(x2, y2, canvas.Width, canvas.Height)
-	size = pct(size, canvas.Width)
-	AbsLine(canvas.Container, int(x1), int(y1), int(x2), int(y2), float32(size), color)
+func (c *Canvas) Line(x1, y1, x2, y2, size float64, color color.RGBA) {
+	x1, y1 = dimen(x1, y1, c.Width, c.Height)
+	x2, y2 = dimen(x2, y2, c.Width, c.Height)
+	size = pct(size, c.Width)
+	AbsLine(c.Container, int(x1), int(y1), int(x2), int(y2), float32(size), color)
 }
 
 // Rect places a rectangle centered on (x,y) within a container, using percent coordinates
-func (canvas *Canvas) Rect(x, y, w, h float64, color color.RGBA) {
-	x, y = dimen(x, y, canvas.Width, canvas.Height)
-	w = pct(w, float64(canvas.Width))
-	h = pct(h, float64(canvas.Height))
-	AbsCornerRect(canvas.Container, int(x-(w/2)), int(y-(h/2)), int(w), int(h), color)
+func (c *Canvas) Rect(x, y, w, h float64, color color.RGBA) {
+	x, y = dimen(x, y, c.Width, c.Height)
+	w = pct(w, float64(c.Width))
+	h = pct(h, float64(c.Height))
+	AbsCornerRect(c.Container, int(x-(w/2)), int(y-(h/2)), int(w), int(h), color)
 }
 
 // CornerRect places a rectangle with upper left corner  on (x,y) within a container, using percent coordinates
-func (canvas *Canvas) CornerRect(x, y, w, h float64, color color.RGBA) {
-	x, y = dimen(x, y, canvas.Width, canvas.Height)
-	w = pct(w, float64(canvas.Width))
-	h = pct(h, float64(canvas.Height))
-	AbsCornerRect(canvas.Container, int(x), int(y), int(w), int(h), color)
+func (c *Canvas) CornerRect(x, y, w, h float64, color color.RGBA) {
+	x, y = dimen(x, y, c.Width, c.Height)
+	w = pct(w, float64(c.Width))
+	h = pct(h, float64(c.Height))
+	AbsCornerRect(c.Container, int(x), int(y), int(w), int(h), color)
 }
 
 // Image places an image centered at (x, y) within a container, using percent coordinates
-func (canvas *Canvas) Image(x, y float64, w, h int, name string) {
-	x, y = dimen(x, y, canvas.Width, canvas.Height)
-	AbsImage(canvas.Container, int(x), int(y), w, h, name)
+func (c *Canvas) Image(x, y float64, w, h int, name string) {
+	x, y = dimen(x, y, c.Width, c.Height)
+	AbsImage(c.Container, int(x), int(y), w, h, name)
 }
