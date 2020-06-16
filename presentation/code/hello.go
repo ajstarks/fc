2065d24af9d11bab5e4da0af7120a35af6d81ea6
Anthony Starks <ajstarks@gmail.com>
2020-06-16 00:44:34 -0400

    add presentation

---
+++ presentation/code/hello.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"image/color"
+
+	"github.com/ajstarks/fc"
+)
+
+func main() {
+	width, height := 500, 500
+	blue := color.RGBA{0, 0, 255, 255}
+	white := color.RGBA{255, 255, 255, 255}
+
+	canvas := fc.NewCanvas("hello", width, height)
+
+	canvas.Circle(50, 0, 100, blue)
+	canvas.CText(50, 25, 10, "hello, world", white)
+	canvas.Image(50, 75, 200, 200, "earth.jpg")
+
+	canvas.EndRun()
+}
