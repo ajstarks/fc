59d8df9ac0d4ed308a30286d9d3c9564553304bd
Anthony Starks <ajstarks@gmail.com>
2021-01-02 19:04:07 -0500

    showimage: add width and height options

--- cmd/showimage/showimage.go
+++ cmd/showimage/showimage.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"image"
 	_ "image/gif"
@@ -13,11 +14,17 @@ import (
 
 func main() {
 	var imagefile string
-	if len(os.Args) < 2 {
+	var imw, imh int
+
+	flag.IntVar(&imw, "w", 0, "image width")
+	flag.IntVar(&imh, "h", 0, "image height")
+	flag.Parse()
+
+	if len(flag.Args()) != 1 {
 		fmt.Fprintln(os.Stderr, "specify an image file")
 		os.Exit(1)
 	}
-	imagefile = os.Args[1]
+	imagefile = flag.Args()[0]
 	f, ferr := os.Open(imagefile)
 	if ferr != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", ferr)
@@ -28,9 +35,15 @@ func main() {
 		fmt.Fprintf(os.Stderr, "%s: %v\n", imagefile, err)
 		os.Exit(3)
 	}
+	if imw == 0 {
+		imw = im.Width
+	}
+	if imh == 0 {
+		imh = im.Height
+	}
 
-	canvas := fc.NewCanvas(imagefile, im.Width, im.Height)
-	canvas.Image(50, 50, im.Width, im.Height, imagefile)
+	canvas := fc.NewCanvas(imagefile, imw, imh)
+	canvas.Image(50, 50, imw, imh, imagefile)
 	f.Close()
 	canvas.EndRun()
 }
